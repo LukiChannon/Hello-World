c3511c45c4eefd9198bba4bfee68e68450d8abc9
LukiChannon <bruckschwaiger@gmail.com>
2025-08-14 08:57:30 +0200

    asdf

--- Project1/script.js
+++ Project1/script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
     const addTaskBtn = document.getElementById('add-task-btn');
     const taskList = document.getElementById('task-list');
+    const emptyImage = document.querySelector('empty-image');
+
+    const toggleEmptyState = () => {
+        emptyImage.computedStyleMap.display = taskList.children.length === 0 ? 'block' : 'none';
+      }
 
     const addTask = (event) => {
         event.preventDefault();
